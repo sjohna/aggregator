9fb814d0d738daa8bd6d01dea1b11e2027f20365
John A. Stevens <john@otherskylines.com>
2021-08-11 19:41:01 -0600

    Add idempotency scenario for AggregatorSystem atom feed processing

--- src/server/AggregatorLibTest/AggregatorSystem/ProcessRawAtomContent.cs
+++ src/server/AggregatorLibTest/AggregatorSystem/ProcessRawAtomContent.cs
@@ -1066,6 +1066,208 @@ namespace AggregatorLibTest.TestAggregatorSystem
             }
         }
 
+        [Test]
+        public void IdempotencyScenario()
+        {
+            var content = RawContentForEmbeddedAtomFeed("singlePost.xml", Instant.FromUnixTimeSeconds(12345678));
+            system.ProcessRawContent(content);
+
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("singlePost.xml", Instant.FromUnixTimeSeconds(99999999)));
+
+            var content2 = RawContentForEmbeddedAtomFeed("twoPosts.xml", Instant.FromUnixTimeSeconds(23456789));
+            system.ProcessRawContent(content2);
+
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("singlePost.xml", Instant.FromUnixTimeSeconds(99999999)));
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("secondPost.xml", Instant.FromUnixTimeSeconds(99999999)));
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("twoPosts.xml", Instant.FromUnixTimeSeconds(99999999)));
+
+            var content3 = RawContentForEmbeddedAtomFeed("threePostsOneUpdated.xml", Instant.FromUnixTimeSeconds(34567890));
+            system.ProcessRawContent(content3);
+
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("singlePost.xml", Instant.FromUnixTimeSeconds(99999999)));
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("secondPost.xml", Instant.FromUnixTimeSeconds(99999999)));
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("twoPosts.xml", Instant.FromUnixTimeSeconds(99999999)));
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("singlePostUpdated.xml", Instant.FromUnixTimeSeconds(99999999)));
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("twoPostsOneUpdated.xml", Instant.FromUnixTimeSeconds(99999999)));
+            system.ProcessRawContent(RawContentForEmbeddedAtomFeed("threePostsOneUpdated.xml", Instant.FromUnixTimeSeconds(99999999)));
+
+            Assert.AreEqual(3, system.RawContentRepository.GetAllRawContent().Count());
+            Assert.AreEqual(5, system.UnprocessedDocumentRepository.GetAllUnprocessedDocuments().Count());
+
+            // first post - first version
+            {
+                // TODO: queries...
+                var doc = system.UnprocessedDocumentRepository
+                    .GetAllUnprocessedDocuments()
+                    .Where(doc => doc.SourceId == "12345")
+                    .OrderBy(doc => doc.UpdateTime)
+                    .First();
+
+                Assert.AreEqual(null, doc.ParentDocumentUri);
+                Assert.AreEqual(Instant.FromUtc(2021, 2, 27, 0, 0, 0), doc.PublishTime);
+                Assert.AreEqual(Instant.FromUtc(2021, 2, 27, 0, 0, 0), doc.UpdateTime);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title", doc.Uri);
+                Assert.AreEqual(Instant.FromUnixTimeSeconds(12345678), doc.RetrieveTime);
+                Assert.AreEqual("12345", doc.SourceId);
+
+                var unprocessedContent = (doc.Content as BlogPostContent)!;
+                Assert.IsNotNull(unprocessedContent);
+
+                Assert.AreEqual("Test Blog Entry Title", unprocessedContent.Title);
+                Assert.AreEqual("<p>This is the test blog entry content.</p>", unprocessedContent.Content);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title/feed/atom/", unprocessedContent.CommentFeedUri);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title/#comments", unprocessedContent.CommentUri);
+                Assert.AreEqual(true, unprocessedContent.AllowsComments);
+                Assert.AreEqual(0, unprocessedContent.Categories.Count());
+
+                Assert.AreEqual(1, doc.Authors.Count());
+
+                var author = doc.Authors.First();
+
+                Assert.AreEqual("Test Blogger", author.Name);
+                Assert.AreEqual("https://example.com/testblog", author.Uri);
+                Assert.AreEqual("blog", author.Context);
+
+                Assert.AreEqual(doc.SourceRawContentId, content.Id);
+            }
+
+            // first post - second version
+            {
+                // TODO: queries...
+                var doc = system.UnprocessedDocumentRepository
+                    .GetAllUnprocessedDocuments()
+                    .Where(doc => doc.SourceId == "12345")
+                    .OrderBy(doc => doc.UpdateTime)
+                    .Skip(1)
+                    .First();
+
+                Assert.AreEqual(null, doc.ParentDocumentUri);
+                Assert.AreEqual(Instant.FromUtc(2021, 2, 27, 0, 0, 0), doc.PublishTime);
+                Assert.AreEqual(Instant.FromUtc(2021, 2, 27, 1, 0, 0), doc.UpdateTime);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title", doc.Uri);
+                Assert.AreEqual(Instant.FromUnixTimeSeconds(34567890), doc.RetrieveTime);
+                Assert.AreEqual("12345", doc.SourceId);
+
+                var unprocessedContent = (doc.Content as BlogPostContent)!;
+                Assert.IsNotNull(unprocessedContent);
+
+                Assert.AreEqual("Test Blog Entry Title", unprocessedContent.Title);
+                Assert.AreEqual("<p>This is the updated test blog entry content.</p>", unprocessedContent.Content);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title/feed/atom/", unprocessedContent.CommentFeedUri);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title/#comments", unprocessedContent.CommentUri);
+                Assert.AreEqual(true, unprocessedContent.AllowsComments);
+                Assert.AreEqual(0, unprocessedContent.Categories.Count());
+
+                Assert.AreEqual(1, doc.Authors.Count());
+
+                var author = doc.Authors.First();
+
+                Assert.AreEqual("Test Blogger", author.Name);
+                Assert.AreEqual("https://example.com/testblog", author.Uri);
+                Assert.AreEqual("blog", author.Context);
+
+                Assert.AreEqual(doc.SourceRawContentId, content3.Id);
+            }
+
+            // second post
+            {
+                // TODO: queries...
+                var doc = system.UnprocessedDocumentRepository
+                    .GetAllUnprocessedDocuments()
+                    .Where(doc => doc.SourceId == "12346")
+                    .OrderBy(doc => doc.UpdateTime)
+                    .First();
+
+                Assert.AreEqual(null, doc.ParentDocumentUri);
+                Assert.AreEqual(Instant.FromUtc(2021, 2, 27, 0, 30, 0), doc.PublishTime);
+                Assert.AreEqual(Instant.FromUtc(2021, 2, 27, 0, 30, 0), doc.UpdateTime);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title-2", doc.Uri);
+                Assert.AreEqual(Instant.FromUnixTimeSeconds(23456789), doc.RetrieveTime);
+                Assert.AreEqual("12346", doc.SourceId);
+
+                var unprocessedContent = (doc.Content as BlogPostContent)!;
+                Assert.IsNotNull(unprocessedContent);
+
+                Assert.AreEqual("Test Blog Entry Title 2", unprocessedContent.Title);
+                Assert.AreEqual("<p>This is the second test blog entry content.</p>", unprocessedContent.Content);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title-2/feed/atom/", unprocessedContent.CommentFeedUri);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title-2/#comments", unprocessedContent.CommentUri);
+                Assert.AreEqual(true, unprocessedContent.AllowsComments);
+                Assert.AreEqual(0, unprocessedContent.Categories.Count());
+
+                Assert.AreEqual(1, doc.Authors.Count());
+
+                var author = doc.Authors.First();
+
+                Assert.AreEqual("Test Blogger", author.Name);
+                Assert.AreEqual("https://example.com/testblog", author.Uri);
+                Assert.AreEqual("blog", author.Context);
+
+                Assert.AreEqual(doc.SourceRawContentId, content2.Id);
+            }
+
+            // third post
+            {
+                // TODO: queries...
+                var doc = system.UnprocessedDocumentRepository
+                    .GetAllUnprocessedDocuments()
+                    .Where(doc => doc.SourceId == "12347")
+                    .OrderBy(doc => doc.UpdateTime)
+                    .First();
+
+                Assert.AreEqual(null, doc.ParentDocumentUri);
+                Assert.AreEqual(Instant.FromUtc(2021, 2, 27, 2, 0, 0), doc.PublishTime);
+                Assert.AreEqual(Instant.FromUtc(2021, 2, 27, 2, 0, 0), doc.UpdateTime);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title-3", doc.Uri);
+                Assert.AreEqual(Instant.FromUnixTimeSeconds(34567890), doc.RetrieveTime);
+                Assert.AreEqual("12347", doc.SourceId);
+
+                var unprocessedContent = (doc.Content as BlogPostContent)!;
+                Assert.IsNotNull(unprocessedContent);
+
+                Assert.AreEqual("Test Blog Entry Title 3", unprocessedContent.Title);
+                Assert.AreEqual("<p>This is the third test blog entry content.</p>", unprocessedContent.Content);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title-3/feed/atom/", unprocessedContent.CommentFeedUri);
+                Assert.AreEqual("https://example.com/testblog/test-blog-entry-title-3/#comments", unprocessedContent.CommentUri);
+                Assert.AreEqual(true, unprocessedContent.AllowsComments);
+                Assert.AreEqual(0, unprocessedContent.Categories.Count());
+
+                Assert.AreEqual(1, doc.Authors.Count());
+
+                var author = doc.Authors.First();
+
+                Assert.AreEqual("Test Blogger", author.Name);
+                Assert.AreEqual("https://example.com/testblog", author.Uri);
+                Assert.AreEqual("blog", author.Context);
+
+                Assert.AreEqual(doc.SourceRawContentId, content3.Id);
+            }
+
+            // title doc
+            {
+                var doc = system.UnprocessedDocumentRepository.GetAllUnprocessedDocuments().First(doc => doc.DocumentType == UnprocessedDocumentType.SourceDescription);
+
+                Assert.AreEqual(null, doc.ParentDocumentUri);
+                Assert.AreEqual(null, doc.PublishTime);
+                Assert.AreEqual(null, doc.UpdateTime);
+                Assert.AreEqual("https://example.com/testblog", doc.Uri);
+                Assert.AreEqual(Instant.FromUnixTimeSeconds(12345678), doc.RetrieveTime);
+                Assert.AreEqual("http://example.com/testblog/feed/atom/", doc.SourceId);
+
+                var unprocessedContent = (doc.Content as FeedSourceDescriptionContent)!;
+                Assert.IsNotNull(unprocessedContent);
+
+                Assert.AreEqual("Test Blog", unprocessedContent.Title);
+                Assert.AreEqual("This is the subtitle for the test blog.", unprocessedContent.Description);
+                Assert.AreEqual("https://example.files.wordpress.com/testblogicon.jpg?w=32", unprocessedContent.IconUri);
+
+                Assert.AreEqual(0, doc.Authors.Count());
+
+                Assert.AreEqual(doc.SourceRawContentId, content.Id);
+            }
+        }
+
+
         // NEXT:
         // (DONE) Test update to existing document
         // (DONE) Test different document
