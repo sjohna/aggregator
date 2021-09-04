5cdb628d211e6b97a0b6efa4c352e6c804d33213
John A. Stevens <john@otherskylines.com>
2021-09-04 17:22:16 -0600

    Update namespace for file copied from AggregatorLib to Aggregator.

--- src/server/Aggregator/UnprocessedDocumentContentJsonConverter.cs
+++ src/server/Aggregator/UnprocessedDocumentContentJsonConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AggregatorLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace AggregatorLib
+namespace Aggregator
 {
     public class UnprocessedDocumentContentJsonConverter : JsonConverter<UnprocessedDocumentContent>
     {
