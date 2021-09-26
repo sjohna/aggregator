cc035481abf85a6c82137b46cf2d93ac05d62b5c
John A. Stevens <john@otherskylines.com>
2021-09-25 19:03:04 -0600

    Initial cut at disabling image loading.

--- src/frontend/src/index.ts
+++ src/frontend/src/index.ts
@@ -55,16 +55,64 @@ function renderDocuments() {
     dateAndAuthorElement.innerText = `${unprocessedDocument.updateTime} ${unprocessedDocument?.authors[0]?.name}`
     documentElement.appendChild(dateAndAuthorElement);
 
-    const contentElement = createElement('div', 'content');
-    contentElement.innerHTML = unprocessedDocument.content.content;
-    documentElement.appendChild(contentElement);
+    documentElement.appendChild(createContentElement(unprocessedDocument));
   }
 
   body.appendChild(containerElement);
 }
 
-function createElement(tag: string, className: string): HTMLElement {
+function createContentElement(doc: UnprocessedDocument) {
+  const contentElement = createElement('div', 'content');
+  contentElement.innerHTML = doc.content.content;
+
+  const imageTags = contentElement.getElementsByTagName('img');
+
+  for (let i = 0; i < imageTags.length; ++i) {
+    const image = imageTags.item(i);
+    
+    // TODO: find a more general way to stopp image loading. I don't like how I have to figure out exactly which attributes to cache.
+    let src: string;
+    if (image.src) {
+      src = image.src;
+      image.src = '#';
+    }
+
+    let srcset: string;
+    if (image.srcset) {
+      srcset = image.srcset;
+      image.srcset = '#';
+    }
+
+
+    const placeholderElement = createElement('div','imagePlaceholder');
+    placeholderElement.style.height = '250px';
+    placeholderElement.style.width = '250px';
+
+    const showPlaceholderDiv = createElement('div');
+    const showPlaceholderButton = createElement('button');
+    showPlaceholderButton.innerText = "Show Image";
+    showPlaceholderButton.onclick = (event) => {
+      //contentElement.replaceChild(image, placeholderElement);
+      placeholderElement.insertAdjacentElement('afterend', image);
+      placeholderElement.remove();
+      if (src) image.src = src;
+      if (srcset) image.srcset = srcset;
+    }
+    showPlaceholderDiv.appendChild(showPlaceholderButton);
+    placeholderElement.appendChild(showPlaceholderDiv);
+    const imageLinkText = createElement('div','imagePlaceholderLinkText');
+    imageLinkText.innerText = srcset;
+    placeholderElement.appendChild(imageLinkText);
+
+    image.insertAdjacentElement('afterend', placeholderElement);
+    image.remove();
+  }
+
+  return contentElement;
+}
+
+function createElement(tag: string, className: string = null): HTMLElement {
   const element = document.createElement(tag);
-  element.classList.add(className);
+  if (className) element.classList.add(className);
   return element;
 }
