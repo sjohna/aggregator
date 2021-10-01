a446d505274561f92bd7fa2357a49672d4409e6a
John A. Stevens <john@otherskylines.com>
2021-09-30 19:36:12 -0600

    Add ability to switch between UnprocessedDocuments and RawContent

--- src/frontend/src/index.ts
+++ src/frontend/src/index.ts
@@ -5,9 +5,17 @@ import { renderDocuments, UnprocessedDocument } from "./unprocessedDocument";
 const UnprocessedDocumentUri = 'api/UnprocessedDocument';
 const RawContentUri = 'api/RawContent';
 
+let mainElement = document.getElementById('main');
+let headerElement = document.getElementById('header');
+let unprocessedDocumentsButton: HTMLElement;
+let rawContentButton: HTMLElement;
+
 let unprocessedDocuments : UnprocessedDocument[];
 let rawContent: RawContent[];
 
+let currentSelection = "UnprocessedDocuments";
+
+configureHeader();
 fetchUnprocessedDocuments();
 fetchRawContent();
 
@@ -15,7 +23,9 @@ async function fetchUnprocessedDocuments() {
   try {
     const uri = 'https://localhost:44365/' + UnprocessedDocumentUri;
     unprocessedDocuments = await (await fetch(uri)).json();
-    //renderDocuments(document.getElementsByTagName('body')[0], unprocessedDocuments);
+    if (currentSelection === "UnprocessedDocuments") {
+      selectionUpdated();
+    }
   } catch (error) {
     console.log(error);
   }
@@ -25,8 +35,64 @@ async function fetchRawContent() {
   try {
     const uri = 'https://localhost:44365/' + RawContentUri;
     rawContent = await (await fetch(uri)).json();
-    renderRawContent(document.getElementsByTagName('body')[0], rawContent);
+    renderRawContent(mainElement, rawContent);
+    if (currentSelection === "RawContent") {
+      selectionUpdated();
+    }
   } catch (error) {
     console.log(error);
   }
+}
+
+function configureHeader() {
+  unprocessedDocumentsButton = createElement('button', 'headerButton', 'headerButtonSelected');
+  rawContentButton = createElement('button', 'headerButton', 'headerButtonUnselected');
+
+  unprocessedDocumentsButton.innerText = "Unprocessed Documents";
+  rawContentButton.innerText = "Raw Content";
+
+  unprocessedDocumentsButton.onclick = () => {
+    if (currentSelection !== "UnprocessedDocuments") {
+      currentSelection = "UnprocessedDocuments";
+      selectionUpdated();
+    }
+  }
+
+  rawContentButton.onclick = () => {
+    if (currentSelection !== "RawContent") {
+      currentSelection = "RawContent";
+      selectionUpdated();
+    }
+  }
+
+  headerElement.appendChild(unprocessedDocumentsButton);
+  headerElement.appendChild(rawContentButton);
+}
+
+function selectionUpdated() {
+  if (currentSelection === "UnprocessedDocuments") {
+    unprocessedDocumentsButton.classList.remove('headerButtonUnselected');
+    unprocessedDocumentsButton.classList.add('headerButtonSelected');
+    rawContentButton.classList.remove('headerButtonSelected');
+    rawContentButton.classList.add('headerButtonUnselected');
+
+    mainElement.innerHTML = '';
+    renderDocuments(mainElement, unprocessedDocuments);
+  } else if (currentSelection === "RawContent") {
+    unprocessedDocumentsButton.classList.add('headerButtonUnselected');
+    unprocessedDocumentsButton.classList.remove('headerButtonSelected');
+    rawContentButton.classList.add('headerButtonSelected');
+    rawContentButton.classList.remove('headerButtonUnselected');
+
+    mainElement.innerHTML = '';
+    renderRawContent(mainElement, rawContent);
+  }
+}
+
+function createElement(tag: string, ...classNames: string[]): HTMLElement {
+  const element = document.createElement(tag);
+  for (const className of classNames) {
+    if (className) element.classList.add(className);
+  }
+  return element;
 }
