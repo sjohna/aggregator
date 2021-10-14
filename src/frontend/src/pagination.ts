7ef110028650d2afcd5c216e8abe990919ef64c5
John A. Stevens <john@otherskylines.com>
2021-10-13 22:33:29 -0600

    Pagination: implement renderPaginationNavigation function
    Apply said function to UnprocessedDocument rendering

--- src/frontend/src/pagination.ts
+++ src/frontend/src/pagination.ts
@@ -1,9 +1,42 @@
 import { Page } from "./page";
+import { createElement } from "./util";
 
-export type Render<T> = (container: HTMLElement, item: T) => void;
+export type Render<T> = (containingElement: HTMLElement, item: T) => void;
 
 export function renderPage<T>(containingElement: HTMLElement, page: Page<T>, renderElement: Render<T>) {
   for (const item of page.items) {
     renderElement(containingElement, item);
   }  
+}
+
+export function renderPaginationNavigation<T>(containerElement: HTMLElement, paginationInfo: PaginationInfo, page: Page<T>, renderPage: () => Promise<void>) {
+  const paginationElement = createElement('div', 'paginationInformation');
+  const infoElement = createElement('span');
+  infoElement.innerText = `Documents ${page.offset + 1} - ${page.offset + page.items.length} of ${page.total}`;
+  paginationElement.appendChild(infoElement);
+  const nextPageButton = createElement('button');
+  nextPageButton.innerText = "Next";
+  nextPageButton.onclick = async () => {
+    if (paginationInfo.offset + paginationInfo.pageSize < paginationInfo.total) {
+      paginationInfo.offset += paginationInfo.pageSize;
+      await renderPage();
+    }
+  }
+  const prevPageButton = createElement('button');
+  prevPageButton.innerText = "Prev";
+  prevPageButton.onclick = async () => {
+    if (paginationInfo.offset - paginationInfo.pageSize >= 0) {
+      paginationInfo.offset -= paginationInfo.pageSize;
+      await renderPage();
+    }
+  }
+  paginationElement.appendChild(prevPageButton);
+  paginationElement.appendChild(nextPageButton);
+  containerElement.appendChild(paginationElement);  
+}
+
+export class PaginationInfo {
+  public offset = 0;
+  public pageSize = 5;
+  public total?: number;
 }
--- src/frontend/src/unprocessedDocument.ts
+++ src/frontend/src/unprocessedDocument.ts
@@ -2,7 +2,7 @@ import { createElement } from "./util";
 import { renderSimpleContainer, SimpleContainerContentType } from "./simpleContainer";
 import { Page } from "./page";
 import { RestRequester } from "./restRequester";
-import { renderPage } from "./pagination";
+import { PaginationInfo, renderPage, renderPaginationNavigation } from "./pagination";
 
 export class UnprocessedDocumentAuthor {
   name: string;
@@ -39,9 +39,7 @@ let documentsElement: HTMLElement;
 let orderByUpdatedCheckbox: HTMLInputElement;
 const UnprocessedDocumentUri = 'api/UnprocessedDocument';
 
-let offset = 0;
-let pageSize = 5;
-let total: number;
+const paginationInfo = new PaginationInfo();
 const requester = new RestRequester<UnprocessedDocument>(UnprocessedDocumentUri);
 
 async function fetchUnprocessedDocuments(pageSize: number, offset: number): Promise<Page<UnprocessedDocument>> {
@@ -50,35 +48,14 @@ async function fetchUnprocessedDocuments(pageSize: number, offset: number): Prom
     orderByUpdatedCheckbox?.checked ? 'UpdateTime' : undefined,
     pageSize,
     offset);
-  total = page.total;
+    paginationInfo.total = page.total;
   return page;
 }
 
 function renderUnprocessedDocumentPage(containingElement: HTMLElement, unprocessedDocuments: Page<UnprocessedDocument>) {
   const containerElement = createElement('div', 'unprocessedDocuments');
-  const paginationElement = createElement('div', 'paginationInformation');
-  const infoElement = createElement('span');
-  infoElement.innerText = `Documents ${unprocessedDocuments.offset + 1} - ${unprocessedDocuments.offset + unprocessedDocuments.items.length} of ${unprocessedDocuments.total}`;
-  paginationElement.appendChild(infoElement);
-  const nextPageButton = createElement('button');
-  nextPageButton.innerText = "Next";
-  nextPageButton.onclick = async () => {
-    if (offset + pageSize < total) {
-      offset += pageSize;
-      await fetchAndRenderDocuments(documentsElement)
-    }
-  }
-  const prevPageButton = createElement('button');
-  prevPageButton.innerText = "Prev";
-  prevPageButton.onclick = async () => {
-    if (offset - pageSize >= 0) {
-      offset -= pageSize;
-      await fetchAndRenderDocuments(documentsElement)
-    }
-  }
-  paginationElement.appendChild(prevPageButton);
-  paginationElement.appendChild(nextPageButton);
-  containerElement.appendChild(paginationElement);
+
+  renderPaginationNavigation(containerElement, paginationInfo, unprocessedDocuments, () => fetchAndRenderDocuments(documentsElement));
 
   renderPage(containerElement, unprocessedDocuments, renderUnprocessedDocumentInSimpleContainer);
 
@@ -117,57 +94,8 @@ export async function renderDocuments(containingElement: HTMLElement) {
 }
 
 async function fetchAndRenderDocuments(containingElement: HTMLElement) {
-  const documents = await fetchUnprocessedDocuments(pageSize,offset);
+  const documents = await fetchUnprocessedDocuments(paginationInfo.pageSize, paginationInfo.offset);
   containingElement.innerHTML = '';
 
   renderUnprocessedDocumentPage(containingElement, documents);
-}
-
-export function createContentElement(doc: UnprocessedDocument) {
-  const contentElement = createElement('div', 'content');
-  contentElement.innerHTML = doc.content.content;
-
-  // const imageTags = contentElement.getElementsByTagName('img');
-
-  // for (let i = 0; i < imageTags.length; ++i) {
-  //   const image = imageTags.item(i);
-    
-  //   // TODO: find a more general way to stopp image loading. I don't like how I have to figure out exactly which attributes to cache.
-  //   let src: string;
-  //   if (image.src) {
-  //     src = image.src;
-  //     image.src = '#';
-  //   }
-
-  //   let srcset: string;
-  //   if (image.srcset) {
-  //     srcset = image.srcset;
-  //     image.srcset = '#';
-  //   }
-
-  //   const placeholderElement = createElement('div','imagePlaceholder');
-  //   placeholderElement.style.height = '250px';
-  //   placeholderElement.style.width = '250px';
-
-  //   const showPlaceholderDiv = createElement('div');
-  //   const showPlaceholderButton = createElement('button');
-  //   showPlaceholderButton.innerText = "Show Image";
-  //   showPlaceholderButton.onclick = (event) => {
-  //     //contentElement.replaceChild(image, placeholderElement);
-  //     placeholderElement.insertAdjacentElement('afterend', image);
-  //     placeholderElement.remove();
-  //     if (src) image.src = src;
-  //     if (srcset) image.srcset = srcset;
-  //   }
-  //   showPlaceholderDiv.appendChild(showPlaceholderButton);
-  //   placeholderElement.appendChild(showPlaceholderDiv);
-  //   const imageLinkText = createElement('div','imagePlaceholderLinkText');
-  //   imageLinkText.innerText = srcset;
-  //   placeholderElement.appendChild(imageLinkText);
-
-  //   image.insertAdjacentElement('afterend', placeholderElement);
-  //   image.remove();
-  // }
-
-  return contentElement;
 }
